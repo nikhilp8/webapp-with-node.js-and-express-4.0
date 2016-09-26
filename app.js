a06bf4ad3017348246e4df134a591e860ccc232b
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-09-26 17:02:28 -0500

    Add books, authors in navbar

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ var app = express();
 var port = 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+// app.use(express.static('src/views'));
+app.set('views','./src/views');               // views are in src/views 
+//app.set('view engine', 'jade');                //view engine is jade
+//app.set('view engine', '.hbs');  
+app.set('view engine', '.ejs');  
+
+
+
+var handlebars = require('express-handlebars');
+app.engine('.hbs',handlebars({extname:'.hbs'}));
 
 app.listen(port, function(err){
     console.log('running server on portnumber '+port);
@@ -14,7 +23,17 @@ app.listen(port, function(err){
 
 
 app.get('/', function(req,res){
-    res.send('Hello World');
+    res.render('index',{
+        title:'hello,I am title',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    
+    });
 });
 
 app.get('/books', function(req,res){
