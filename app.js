da9a4c04f8f1c7dc9c1cb0929a2c8df9a924cc9a
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-09-27 14:05:48 -0500

    Add book routes and render books page

--- app.js
+++ app.js
@@ -22,9 +22,66 @@ app.listen(port, function(err){
     console.log('running server on portnumber '+port);
 });
 
+var books = [
+    {
+        title:'India After Gandhi',
+        genre:'History',
+        author:'Ramchandra Guha',
+        read:false
+    },
+     {
+        title:'Rich Dad Poor Dad',
+        genre:'fiction',
+        author:'Robert Kioski',
+        read:false
+    },
+     {
+        title:'The White Tiger',
+        genre:'fiction',
+        author:'Aravind Adiga',
+        read:false
+    },
+     {
+        title:'The Glass Palace',
+        genre:'fiction',
+        author:'Amitav Ghosh',
+        read:false
+    },
+     {
+        title:'The encyclopedia of Bodybuilding',
+        genre:'education',
+        author:'Arnold Schwazenegger',
+        read:false
+    },
+     {
+        title:'Quiet',
+        genre:'biography',
+        author:'Susan Caine',
+        read:false
+    },
+     {
+        title:'Kim',
+        genre:'fiction',
+        author:'Rudyard Kipling',
+        read:false
+    }
+    
+];
+
 bookRouter.route('/')
     .get(function(req,res){
-        res.send('Hello Books')
+        res.render('books',{
+        title:'hello,I am title',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }],
+        books: books
+    
+    })
 });
 bookRouter.route('/single')
     .get(function(req,res){
@@ -32,6 +89,7 @@ bookRouter.route('/single')
 });
 
 app.use('/Books', bookRouter);
+
 app.get('/', function(req,res){
     res.render('index',{
         title:'hello,I am title',
