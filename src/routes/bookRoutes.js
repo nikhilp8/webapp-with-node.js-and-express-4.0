d9098ab48d7ffb2088744b404bae2ad463b2cc8f
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-09-30 12:16:42 -0500

    Select books for bookRoutes from the created DB

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,61 +1,30 @@
 var express = require('express');
 var bookRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
 
 var router = function(nav) {
-    var books = [
-    {
-        title:'India After Gandhi',
-        genre:'History',
-        author:'Ramchandra Guha',
-        read:false
-    },
-     {
-        title:'Rich Dad Poor Dad',
-        genre:'fiction',
-        author:'Robert Kioski',
-        read:false
-    },
-     {
-        title:'The White Tiger',
-        genre:'fiction',
-        author:'Aravind Adiga',
-        read:false
-    },
-     {
-        title:'The Glass Palace',
-        genre:'fiction',
-        author:'Amitav Ghosh',
-        read:false
-    },
-     {
-        title:'The encyclopedia of Bodybuilding',
-        genre:'education',
-        author:'Arnold Schwazenegger',
-        read:false
-    },
-     {
-        title:'Quiet',
-        genre:'biography',
-        author:'Susan Caine',
-        read:false
-    },
-     {
-        title:'Kim',
-        genre:'fiction',
-        author:'Rudyard Kipling',
-        read:false
-    }
-    
-];
+   
 
     bookRouter.route('/')
     .get(function(req,res){
-        res.render('bookListView',{
-        title:'hello,I am title',
-        nav: nav,
-        books: books
+   
+        var url = 'mongodb://localhost:27017/libraryApp';
+        mongodb.connect(url, function(err, db){
+            var collection = db.collection('books');
+            collection.find({}).toArray(function(err,results){
+                res.render('bookListView',{
+                title:'hello,I am title',
+                nav: nav,
+                books: results
     
                 })
+            })
+
+        
+        });
+        
+        
     });
 
     bookRouter.route('/:id')
