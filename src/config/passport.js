96a8f7f2ff0913055cce5b4020c05ead39137615
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-10-07 16:16:28 -0500

    Add passport serializer and deserializer

--- src/config/passport.js
+++ src/config/passport.js
@@ -3,4 +3,11 @@ var passport = require('passport');
 module.exports = function(nav) {
     app.use(passport.initialize());
     app.use(passport.session());
+    
+    passport.serializer(function(user,done){
+        done(null, user);
+    });
+    passport.deserializer(function(userId,done){
+        done(null, user);
+    });
 }
